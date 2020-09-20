1d5650e70484da95c17569d6e26278cd59824190
dolmagrg1231 <dolmagurung75@gmail.com>
2020-09-20 17:02:18 -0400

    added sessions

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template, url_for, redirect
-
+from flask import Flask, render_template, url_for, redirect, request, session
+from datetime import timedelta
 #referencing this file
 app = Flask(__name__)
+app.secret_key = "hello"
+app.permanent_session_lifetime = timedelta(minutes=5)
 
 #pass the url string
 @app.route("/")
@@ -11,12 +13,29 @@ def home():
 
 @app.route("/login", methods=["POST", "GET"])
 def login():
-    return render_template("login.html")
+    if request.method =="POST":
+        session.permanent = True
+        user = request.form["nm"]
+        session["user"] = user
+        return redirect(url_for("user"))
+    else:
+        if "user" in session:
+            return redirect(url_for("user"))
 
-@app.route("/<usr>")
-def user(usr):
-    return f"<h1>{usr}</h1>"
+        return render_template("login.html")
 
+@app.route("/user")
+def user():
+    if "user" in session:
+        user = session["user"]
+        return f"<h1>{user}</h1>"
+    else:
+        return redirect(url_for("login"))
+
+@app.route('/logout')
+def logout():
+    session.pop("user", None)
+    return redirect(url_for("login"))
 # @app.route("/test")
 # #define the function for the route
 # def test():
