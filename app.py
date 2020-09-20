6f4f4db68ba08c33cb42fe481e39e3574e486ebc
dolmagrg1231 <dolmagurung75@gmail.com>
2020-09-20 18:24:43 -0400

    Adding database

--- app.py
+++ app.py
@@ -1,9 +1,25 @@
 from flask import Flask, render_template, url_for, redirect, request, session, flash
 from datetime import timedelta
+from flask_sqlalchemy import SQLAlchemy
+
 #referencing this file
 app = Flask(__name__)
 app.secret_key = "hello"
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///users.sqlite3'
+app.config["SQLALCHEMY_TRACK_MODIFICATION"] =False
 app.permanent_session_lifetime = timedelta(minutes=5)
+
+db = SQLAlchemy(app)
+
+class users(db.Model):
+    _id = db.Column("id", db.Integer, primary_key=True)
+    name = db.Column( db.String(100))
+    email = db. Column( db.String(100))
+
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+
 
 #pass the url string
 @app.route("/")
@@ -17,6 +33,15 @@ def login():
         session.permanent = True
         user = request.form["nm"]
         session["user"] = user
+
+        found_user = users.query.filter_by(name=user).first()
+        if found_user:
+            session["email"] = found_user.email
+        else:
+            usr = users(user, "")
+            db.session.add(usr)
+            db.session.commit()
+
         flash("Login Successful")
         return redirect(url_for("user"))
     else:
@@ -25,11 +50,25 @@ def login():
             return redirect(url_for("user"))
         return render_template("login.html")
 
-@app.route("/user")
+@app.route("/user", methods=["POST", "GET"])
 def user():
+    email=None
     if "user" in session:
         user = session["user"]
-        return render_template("user.html", user=user)
+        
+        if request.method == "POST":
+            email = request.form["email"]
+            session["email"] = email
+            found_user = users.query.filter_by(name=user).first()
+            found_user.email = email
+            db.session.commit()
+
+            flash("Email was saved")
+        else:
+            if "email" in session:
+                email = session["email"]
+
+        return render_template("user.html", email=email)
     else:
         flash("not logged in")
         return redirect(url_for("login"))
@@ -41,6 +80,7 @@ def logout():
         user = session["user"]
         flash ("You have been logged out!", "info")
     session.pop("user", None)
+    session.pop("email", None)
     
     return redirect(url_for("login"))
 # @app.route("/test")
@@ -49,7 +89,8 @@ def logout():
 #     return render_template("new.html")
 
 if __name__ == "__main__":
-        app.run(debug=True)
+    db.create_all()
+    app.run(debug=True)
 
 
 # def home():
@@ -67,21 +108,5 @@ if __name__ == "__main__":
 
 
 
-# #initializing a database
-# app.config['SQLALCHEMY_DATABSE_URI'] = 'sqlite:///test.db'
-# db = SQLAlchemy(app)
-
-# #create a model
-# class Todo(db.Model):
-#     #setting id column that specifies id of each entry
-#     id = db.Column(db.Integer, primary_key=True)
-
-#     #text column called content that holds each task
-#     content = db.Column(db.String(200), nullable=False)
-#     date_created = db. column(db.DateTime, default=datetime.utcnow)
-
-#     #return string everytime we create a new element
-#     def __repr__(self):
-#         return '<Task %r>' % self.id
 
 
